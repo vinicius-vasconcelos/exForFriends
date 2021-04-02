48692261a61c7f670219a731071dbb0226bafb2a
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-04-02 15:03:46 -0300

    Arrumando view engine

--- agendaCrudMvcEjs/index.js
+++ agendaCrudMvcEjs/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('viwe engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.urlencoded({ extended: true }));
