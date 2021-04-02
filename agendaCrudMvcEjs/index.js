15e96f6063a3432efe13448533b6abf95b820fac
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-04-02 14:59:08 -0300

    Criando rota de index

--- agendaCrudMvcEjs/index.js
+++ agendaCrudMvcEjs/index.js
@@ -8,4 +8,6 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 
+app.get('/', (_req, res) => res.status(200).render('index', { page: { title: 'Login' } }));
+
 app.listen(3000, () => console.log('Run http://localhost:3000'));
